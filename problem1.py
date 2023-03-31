d5f7cd1d4daf4dea54322c5d6c5c542819fef241
dellt1 <tsinghmathoda@gmail.com>
2023-03-31 09:41:18 -0400

    v2

--- problem1.py
+++ problem1.py
@@ -7,3 +7,8 @@ disc1=0.05
 # more than $300, give a 10% discount
 disc2=0.1
 tax_rate=0.15
+#getting the inputs 
+name=input("enter the patient's name :")
+cleaning= input("was cleaning performed ?")
+cavity=input("was cavity filling performed ?")
+xray=input("was X ray  performed ?")
