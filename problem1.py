8a2481bcd8cde076a335a37cfeeba1e3b75214f9
dellt1 <tsinghmathoda@gmail.com>
2023-03-31 09:31:42 -0400

    v1

---
+++ problem1.py
@@ -0,0 +1,9 @@
+#given values
+cleaning_rate=60
+cavity_filling=200
+x_ray=100
+#when the bill of a patient is more than $200, give the user 5% discount
+disc1=0.05
+# more than $300, give a 10% discount
+disc2=0.1
+tax_rate=0.15
